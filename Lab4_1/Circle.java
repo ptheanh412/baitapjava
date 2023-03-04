cfbb4f21580504ea1dde5ce1da0a4b838883178b
ptheanh412 <pham041203theanh@gmail.com>
2023-03-04 23:42:37 +0700

    lab4

---
+++ Lab4_1/Circle.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lab4;
+
+/**
+ *
+ * @author 84974
+ */
+public class Circle {
+    double radius;
+    String color;
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" + "radius=" + radius + ", color=" + color + '}';
+    }
+    
+    public double getArea(){
+        return this.radius * this.radius * 4 * 3.14;
+    }
+}
